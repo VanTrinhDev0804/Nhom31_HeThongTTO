ef34b6ea13ef6e02bf34cdcccba361829d0626a2
VanTrinhDev0804 <vantrinh2804@gmail.com>
2022-09-07 09:57:43 +0700

    create source and database

---
+++ src/app/FrmLogin.java
@@ -0,0 +1,14 @@
+package app;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+
+public class FrmLogin extends JFrame {
+	public FrmLogin() {
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		getContentPane().add(lblNewLabel, BorderLayout.NORTH);
+	}
+
+}
